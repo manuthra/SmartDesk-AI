84280112645695f6ab18b2a76c8f63ad4ae4a9b5
unknown <manuthramohan@gmail.com>
2026-05-31 22:44:18 +0530

    fix CORS manual headers

--- server/server.js
+++ server/server.js
@@ -10,7 +10,15 @@ import Ticket from "./models/Ticket.js";
 dotenv.config();
 
 const app = express();
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.options("*", cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
